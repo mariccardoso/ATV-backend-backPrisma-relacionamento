9b4dd272604ef9ac65c5bea0f6aa850c5a0f0335
Mariana Cardoso <158165810+mariccardoso@users.noreply.github.com>
2025-04-17 07:50:54 -0300

    Initial commit

---
+++ src/controllers/tarefaController.js
@@ -0,0 +1,40 @@
+import tarefaModel from "../models/tarefaModel.js";
+class TarefaController {
+  getAll = async (req, res) => {
+    try {
+      const tarefas = await tarefaModel.getAll();
+      res.json(tarefas);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ erro: "Erro ao buscar tarefas" });
+    }
+  };
+  create = async (req, res) => {
+    const { descricao } = req.body;
+    try{
+      if (!descricao) {
+        return res.status(400).json({ erro: "Descrição é obrigatória" });
+      }
+      const novaTarefa = await tarefaModel.create(descricao);
+      res.status(201).json(novaTarefa);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ erro: "Erro ao criar tarefa "});
+    }
+  };
+  update = ({ params: { id }, body: { concluida } }, res) => {
+    const tarefaAtualizada = tarefaModel.update(id, concluida);
+    if (!tarefaAtualizada) {
+      return res.status(404).json({ erro: "Tarefa não encontrada" });
+    }
+    res.json(tarefaAtualizada);
+  };
+  delete = ({ params: { id } }, res) => {
+    const sucesso = tarefaModel.delete(id);
+    if (!sucesso) {
+      return res.status(404).json({ erro: "Tarefa não encontrada" });
+    }
+    res.status(204).send();
+  };
+}
+export default new TarefaController();
---
+++ src/models/tarefaModel.js
@@ -0,0 +1,31 @@
+import prisma from "../../prisma/client.js";
+
+class TarefaModel {
+  getAll = async () => {
+    return await prisma.task.findMany();
+  };
+  create = async (descricao) => {
+    return await prisma.task.create({
+      data: {
+        descricao
+      },
+    });
+  };
+  update = async (id, concluida) => {
+    const tarefa = this.tarefas.find((t) => t.id === Number(id));
+    if (tarefa) {
+      tarefa.concluida = concluida !== undefined ? concluida : tarefa.concluida;
+      return await tarefa;
+    }
+    return null;
+  };
+  delete = (id) => {
+    const index = this.tarefas.findIndex((t) => t.id === Number(id));
+    if (index !== -1) {
+      this.tarefas.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
+}
+export default new TarefaModel();

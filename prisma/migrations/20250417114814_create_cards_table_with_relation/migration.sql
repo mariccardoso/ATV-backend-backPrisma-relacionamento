051cc29bb49bd150b3ae3db7ff5b0bde47b61b73
Mariana Cardoso <mariana.c.cardoso@aluno.senai.br>
2025-04-24 07:46:21 -0300

    feat: create project

--- prisma/migrations/20250417114814_create_cards_table_with_relation/migration.sql
+++ prisma/migrations/20250417114814_create_cards_table_with_relation/migration.sql
@@ -1,15 +1,16 @@
 -- CreateTable
 CREATE TABLE "cards" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "rarity" TEXT NOT NULL,
     "attackPoints" INTEGER NOT NULL,
     "defensePoints" INTEGER NOT NULL,
     "imageUrl" TEXT,
     "collectionId" INTEGER NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    CONSTRAINT "cards_collectionId_fkey" FOREIGN KEY ("collectionId") REFERENCES "collections" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "cards_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -17,3 +18,6 @@ CREATE UNIQUE INDEX "cards_name_key" ON "cards"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "cards_collectionId_key" ON "cards"("collectionId");
+
+-- AddForeignKey
+ALTER TABLE "cards" ADD CONSTRAINT "cards_collectionId_fkey" FOREIGN KEY ("collectionId") REFERENCES "collections"("id") ON DELETE CASCADE ON UPDATE CASCADE;

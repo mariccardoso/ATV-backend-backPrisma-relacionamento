ff677d5e897e14749d68f26501e42edec9e2c8cb
Mariana Cardoso <mariana.c.cardoso@aluno.senai.br>
2025-04-29 11:02:28 -0300

    feat: create user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import userModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 class AuthController {
     // Listar todos os usuários
@@ -38,7 +39,7 @@ class AuthController {
                 name,
                 email,
                 password: hashedPassword,
-            }
+            };
 
             // Criar o usuário
             const user = await userModel.create(data);
@@ -47,9 +48,57 @@ class AuthController {
                 message: "Usuário criado com sucesso.",
                 user,
             });
-        } catch (error) { 
+        } catch (error) {
             console.error("Erro ao registrar usuário:", error);
-            return res.status(500).json({ message: "Erro ao registrar usuário." }); 
+            return res.status(500).json({ message: "Erro ao registrar usuário." });
+        }
+    }
+
+    async login(req, res) {
+        try {
+            const { email, password } = req.body;
+
+            // Validar os dados de entrada
+            if (!email || !password) {
+                return res
+                    .status(400)
+                    .json({ message: "Email e senha são campos obrigatórios." });
+            }
+
+            // Verificar se o usuário existe
+            const userExists = await userModel.findByEmail(email);
+            if (!userExists) {
+                return res.status(401).json({ message: "Credenciais inválidas" });
+            }
+
+            // Verificar a senha
+            const isPasswordValid = await bcrypt.compare(
+                password,
+                userExists.password
+            );
+            if (!isPasswordValid) {
+                return res.status(401).json({ message: "Credenciais inválidas" });
+            }
+
+            // Gerar o token JWT
+            const token = jwt.sign(
+                {
+                    id: userExists.id,
+                    email: userExists.email,
+                    name: userExists.name,
+                },
+                process.env.JWT_SECRET,
+                { expiresIn: "24h" }
+            );
+
+            return res.json({
+                message: "Login realizado com sucesso.",
+                token,
+                userExists,
+            });
+        } catch (error) { 
+            console.error("Erro ao fazer login:", error);
+            res.status(500).json({ message: "Erro ao fazer login." });
         }
     }
 }

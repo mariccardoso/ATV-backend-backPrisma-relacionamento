5a3b83c947c497ac7448e5d11da1b81fcf9c60b5
Mariana Cardoso <mariana.c.cardoso@aluno.senai.br>
2025-04-29 08:57:18 -0300

    feat: create user controller

---
+++ src/controllers/authController.js
@@ -0,0 +1,57 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+
+class AuthController {
+    // Listar todos os usuários
+    async getAllUsers(req, res) {
+        try {
+            const users = await UserModel.findAll();
+            res.json(users);
+        } catch (error) {
+            console.error("Erro ao listar usuários:", error);
+            res.status(500).json({ error: "Erro ao listar usuários" });
+        }
+    }
+
+    async register(req, res) {
+        try {
+            const { name, email, password } = req.body;
+
+            // Validar os dados de entrada
+            if (!name || !email || !password) {
+                return res
+                    .status(400)
+                    .json({ message: "Todos os campos são obrigatórios." });
+            }
+
+            // Verificar se o usuário já existe
+            const userExists = await userModel.findByEmail(email);
+            if (userExists) {
+                return res.status(400).json({ message: "Usuário já existe." });
+            }
+
+            // Hash da senha
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Criar objeto do usuário
+            const data = {
+                name,
+                email,
+                password: hashedPassword,
+            }
+
+            // Criar o usuário
+            const user = await userModel.create(data);
+
+            return res.status(201).json({
+                message: "Usuário criado com sucesso.",
+                user,
+            });
+        } catch (error) { 
+            console.error("Erro ao registrar usuário:", error);
+            return res.status(500).json({ message: "Erro ao registrar usuário." });
+        }
+    }
+}
+
+export default new AuthController();
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,23 +4,40 @@ class UserModel {
     // obter todos os usuários
     async findAll() {
         const users = await prisma.user.findMany();
-    
+
         return users;
     }
+
     // obter um usuário pelo id
-    async findAll() {
-        const users = await prisma.user.findMany();
-    
-        return users;
+    async findById(id) {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: Number(id),
+            },
+        });
+
+        return user;
+    }
+
+    //obter usuario pelo email
+    async findByEmail(email) {
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+
+        return user;
     }
     // criar um usuário
     async create(data) {
         const user = await prisma.user.create({
             data,
         });
-    
+
         return user;
     }
+
     // atualizar um usuário
     async update(id, data) {
         const user = await prisma.user.update({
@@ -29,9 +46,10 @@ class UserModel {
             },
             data
         });
-    
+
         return user;
     }
+
     // deletar um usuário
     async delete(id) {
         await prisma.user.delete({
@@ -39,7 +57,7 @@ class UserModel {
                 id: Number(id)
             }
         });
-    
+
         return true;
     }
 }
